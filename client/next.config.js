567bb9afbf33748041f3823bc647d49e8255a6b1
yilmaz <yilmazapis@gmail.com>
2021-10-05 14:31:20 -0400

    finalziing the app

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        web3: false,
+      };
+    }
     config.experiments = { topLevelAwait: true };
     return config;
   },
